c90f6dcbc67d6a0f728468fe40a80e2442967325
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-03-28 18:58:31 -0500

    Update migrate dependencies

--- contents/migrations/0001_initial.py
+++ contents/migrations/0001_initial.py
@@ -18,6 +18,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('tools', '0001_initial'),
+        ('menus', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
