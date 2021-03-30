6213c829cf589b29fccb2b322f94c02224681032
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-03-30 15:45:57 -0500

    Fix production settings

--- projectCato/settings/production.py
+++ projectCato/settings/production.py
@@ -34,7 +34,6 @@ if USE_S3:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = None
 
     # S3 static files
@@ -45,4 +44,4 @@ if USE_S3:
     # S3 media files
     PUBLIC_MEDIA_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'projectCato.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'projectCato.storage_backends.StaticStorage'
