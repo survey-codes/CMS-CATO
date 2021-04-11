e09a317bc7412b36e3b27c0c3433d9db482b55ea
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-04-11 02:24:24 -0500

    Add celery tasks module (first approach)

--- domain/contents/tasks.py
+++ domain/contents/tasks.py
@@ -8,16 +8,20 @@ from domain.contents import models
 logger = get_logger(__name__)
 
 
-@task(name='update_page_jsonfield')
+@task(name='update_page_jsonfield', ignore_result=True)
 def update_page_jsonfield(translation_id):
+    """
+
+    """
     try:
         page_translation = models.PageLanguage.objects.get(pk=translation_id)
         logger.debug(f'Found translation {page_translation}')
+        page_translation.metadata = {
+            'test': 'SUCCESS'
+        }
+        page_translation.save(run_tasks=False, update_fields=['metadata'])
+
     except Exception as e:
         logger.warning(f'Error retrieving translation: \n{e}')
-
-    page_translation.metadata = {
-        'test': 'SUCCESS'
-    }
 
 
