ed8c52f957d5a440fe21ee0d739327b2461a5503
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-04-13 00:03:11 -0500

    Fix query resolvers

--- presentation/contents/api/schema.py
+++ presentation/contents/api/schema.py
@@ -6,50 +6,25 @@ from presentation.contents.api.scalars import JSONCustom
 
 class GeneralInfoQuery(graphene.ObjectType):
     general_info = JSONCustom(
-        lang=graphene.String(description='Obtener información general en un idioma específico')
+        lang=graphene.String(description='Filtrar búsqueda por idioma')
     )
 
     def resolve_general_info(self, info, **kwargs):
         lang = kwargs.get('lang', 'ES')
-        data = {
-            'header': dict(),
-            'footer': None
-        }
 
-        general_info = models.GeneralData.objects.filter(slug_general_data='general-data').first()
-        assert general_info, 'No existe información general'
+        info_translations = models.GeneralDataLanguage.objects.all()
+        assert info_translations.exists(), 'No existe información general del sitio'
 
-        menu = models.Menu.objects.filter(general=True).first()
-        assert general_info, 'No existe un menú general'
+        if lang:
+            info = info_translations.select_related('language').filter(language__abbreviation=lang)
+            if info.exists():
+                return info.first().metadata
 
-        info_lang = general_info.general_data_lang.filter(language__abbreviation=lang).first()
-        if info_lang:
-            data['footer'] = info_lang.general_metadata['footer']
-            menu_lang = menu.menu_lang.filter(language__abbreviation=lang).first()
-
-            if menu_lang:
-                data['header'] = {
-                    'logo': info_lang.general_metadata['logo'],
-                    'menu': menu_lang.menu_metadata['menu']
-                }
-            else:
-                menu_lang = menu.menu_lang.filter(language__abbreviation='ES').first()
-                data['header'] = {
-                    'logo': info_lang.general_metadata['logo'],
-                    'menu': menu_lang.menu_metadata['menu']
-                }
+        # Default language translation
         else:
-            info_lang = general_info.general_data_lang.filter(
-                language__abbreviation='ES'
-            ).first()
-            menu_lang = menu.menu_lang.filter(language__abbreviation='ES').first()
-            data['footer'] = info_lang.general_metadata['footer']
-            data['header'] = {
-                'logo': info_lang.general_metadata['logo'],
-                'menu': menu_lang.menu_metadata['menu']
-            }
-
-        return data
+            info = info_translations.filter(language__abbreviation='ES')
+            assert info.exists(), f'No se encontró ninguna traducción para la información general del sitio'
+            return info.first().metadata
 
 
 class PageQuery(graphene.ObjectType):
@@ -60,19 +35,19 @@ class PageQuery(graphene.ObjectType):
 
     def resolve_page(self, info, **kwargs):
         slug = kwargs.get('slug', None)
-        lang = kwargs.get('lang', 'ES')
+        lang = kwargs.get('lang', None)
         assert slug, 'Debe proporcionar al menos el slug de la página'
 
-        page = models.Page.objects.filter(slug_page=slug).first()
-        assert page, f'No se encontró la página con slug {slug}:'
+        page_translations = models.PageLanguage.objects.filter(page__slug=slug)
+        assert page_translations.exists(), f'No existe la página {slug}'
 
-        page_lang = page.page_lang.filter(language__abbreviation=lang).first()
-        if page_lang:
-            data = page_lang.page_metadata
+        if lang:
+            data = page_translations.objects.filter(language__abbreviation=lang)
+            if data.exists():
+                return data.first().metadata
 
-        # Return the default language translation
+        # Default language translation
         else:
-            page_lang = page.page_lang.filter(language__abbreviation='ES').first()
-            data = page_lang.page_metadata
-
-        return data
+            data = models.PageLanguage.objects.filter(language__abbreviation='ES')
+            assert data.exists(), f'No se encontró ninguna traducción para la página {slug}'
+            return data.first().metadata
