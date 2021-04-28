9eca6c9b7ec97ebe77b90593c77b5a0b75a1f416
Felipe Velásquez <39866404+FelipeVelasquez1802@users.noreply.github.com>
2021-04-28 18:42:58 -0500

    Bugfix/solving audit and mptt issues (#11)
    * GRAL: "Se refactorizan los modelos y se resuelven conflictos"
    
    * GRAL: "
    - Se dividen los modelos de audit.py y de language_abstract.py para evitar circularidades y se añade auditoria en language.py
    - Se modifica la constante usuada en language.py para app_label por la que usan los demás modulos de tools.
    - Se re-ubican las constantes de mail.py donde corresponde
    "
    
    * GRAL: "
    - Se dividen los modelos de audit.py y de language_abstract.py para evitar circularidades y se añade auditoria en language.py
    - Se modifica la constante usuada en language.py para app_label por la que usan los demás modulos de tools.
    - Se re-ubican las constantes de mail.py donde corresponde.
    - Se re-ubican constantes que no se tuvieron en cuenta en quota.py
    - Se re-ubican constantes de template.py y template_admin.py
    - Se re-ubican constantes de user_pqrd.py
    - Se re-organiza toda la app de menus, dejando todo de una manera mucho más accequible
    - Se configuran los inlinen base, audit y language, para generalizar todo
    - Se modifican las funiones de vista para que sean mucho más optimas
    - Se configuran los inline de item menu
    "
    
    * GRAL: "
    - Se configura el administrador de pages con sus respectivos inline
    - Se configura el administrador de secciones y sus respectivos inline
    "
    
    Co-authored-by: andres.diaz <Pipe1802>

--- presentation/contents/admin/admin.py
+++ presentation/contents/admin/admin.py
@@ -9,46 +9,29 @@ from import_export.admin import ImportExportModelAdmin
 from mptt.admin import DraggableMPTTAdmin
 from rangefilter.filter import DateRangeFilter
 
-from infrastructure.data_access.entities.contents.models import pages
 from infrastructure.data_access.entities.contents.models import posts, sections
 from infrastructure.data_access.entities.main.image import get_image_preview
 from presentation import constants as c
+from presentation.constants import NO_PAGES, IMAGE_PREVIEW
 from presentation.contents.resources import PostLanguageResource, ImportPostResource, ExportPostResource
 from presentation.main.admin.admin import Audit2Admin
 
-PAGE = _('Page')
-POSTS = _('Posts')
-SECTION_BACKGROUND_COLOR = _("Section background color")
 COUNT_SECTIONS = _('Number of sections')
 COUNT_IMAGES = _('Number of images in gallery')
 COUNT_TRANSLATIONS = _('Number of translations')
-VISIBILITY_HELP_TEXT = _('Check the box if the posts component is visible in this section')
+
 LOGO_PREVIEW = _('Logo')
 EDIT_TEXT = _('Edit in another window')
-POST_SETTINGS_PLURAL = _("Posts Settings")
-CHOOSE_POSTS = _('Order posts')
-SECTION = _('Section')
-SECTION_PLURAL = _('Sections')
-IMAGE_PREVIEW = _("Preview")
+
 ICON_PREVIEW = _('Icon')
 GALLERIES = _('Galleries')
-NO_POSTS = _("It has no posts")
-NO_PAGES = _("It is not associated with any page")
-NO_SECTIONS = _("It does not contain sections")
+
 SHOW_PAGES = _('Sections and pages in which appears ')
-SHOW_PAGES_SECTION = _('Pages in which appears ')
+
 NO_POST_SECTIONS = _("This post is not contained in any section")
 
 
 # ---------INLINES---------
-
-
-class PageLanguageInline(admin.StackedInline):
-    model = pages.PageLanguage
-    extra = 0
-    suit_classes = 'suit-tab suit-tab-language'
-
-
 class PostLanguageInline(admin.StackedInline):
     model = posts.PostLanguage
     extra = 1
@@ -89,12 +72,6 @@ class SectionInline(SortableInlineAdminMixin, admin.TabularInline):
     raw_id_fields = ('section',)
 
 
-class SectionLanguageInline(admin.StackedInline):
-    model = sections.SectionLanguage
-    extra = 1
-    suit_classes = 'suit-tab suit-tab-language'
-
-
 # ---------ADMIN---------
 
 
@@ -106,116 +83,6 @@ class PostLanguageAdmin(ImportExportModelAdmin):
 
     def has_add_permission(self, request):
         return False
-
-
-@admin.register(sections.Section)
-class SectionAdmin(admin.ModelAdmin):
-    actions = ['update_json_content']
-    list_display = (
-        'title', '_background_preview', '_background_color', 'active', 'creation_date', '_show_pages', '_show_posts'
-    )
-    search_fields = ('creation_date',)
-    list_filter = (('creation_date', DateRangeFilter),)
-    suit_list_filter_horizontal = ('creation_date',)
-    change_list_template = 'admin/change_date_filter.html'
-    inlines = [SectionLanguageInline, PostSettingsInline, ]
-    readonly_fields = ('slug', '_background_preview',) + Audit2Admin.readonly_fields
-    fieldsets = (
-        (SECTION, {
-            'classes': ('suit-tab suit-tab-general',),
-            'fields': ('active', 'title', 'background', '_background_preview', 'background_color',
-                       'slug') + Audit2Admin.fieldsets
-        }),
-        (POST_SETTINGS_PLURAL, {
-            'classes': ('wide', 'suit-tab suit-tab-general'),
-            'description': VISIBILITY_HELP_TEXT,
-            'fields': ('template', 'align')
-        }),
-    )
-    suit_form_tabs = (
-        ('general', 'General'),
-        ('language', c.LANGUAGE_TAB),
-        ('post_settings', CHOOSE_POSTS),
-    )
-
-    def _background_color(self, obj):
-        background = obj.background_color
-        if background:
-            return mark_safe(f'<div style="background-color:{background}; height:50px; width: 50px"></div>')
-        else:
-            return '-'
-
-    _background_color.short_description = SECTION_BACKGROUND_COLOR
-
-    def _show_pages(self, obj):
-        pages = obj.pages.all()
-        html = ""
-        if pages:
-            for page in pages:
-                html += f'<li><a href="/admin/contents/page/{page.id}/">{page}</a></li>'
-                return mark_safe(f'<ul>{html}</ul>')
-        return NO_PAGES
-
-    _show_pages.short_description = SHOW_PAGES_SECTION
-
-    def _background_preview(self, obj):
-        image = obj.background
-        if image:
-            return get_image_preview(obj, img=image.url, title=IMAGE_PREVIEW, )
-        return '-'
-
-    _background_preview.allow_tags = True
-    _background_preview.short_description = IMAGE_PREVIEW
-
-    def _show_posts(self, obj):
-        posts = obj.posts.all()
-        html = ""
-        if posts:
-            for post in posts:
-                html += f'<li><a href="/admin/contents/post/{post.id}/">{post}</a></li>'
-            return mark_safe(f'<ul>{html}</ul>')
-        return NO_POSTS
-
-    _show_posts.short_description = POSTS
-
-
-@admin.register(pages.Page)
-class PageAdmin(DraggableMPTTAdmin, NonSortableParentAdmin):
-    DraggableMPTTAdmin.indented_title.short_description = 'Title'
-    list_display = ('tree_actions', 'indented_title', 'slug', 'active', 'creation_date', '_show_sections')
-    search_fields = ('creation_date',)
-    list_filter = (('creation_date', DateRangeFilter),)
-    suit_list_filter_horizontal = ('creation_date',)
-    change_list_template = 'admin/change_date_filter.html'
-    inlines = [SectionInline, PageLanguageInline]
-    readonly_fields = ('slug',) + Audit2Admin.readonly_fields
-    fieldsets = (
-        (PAGE, {
-            'classes': ('suit-tab suit-tab-page',),
-            'fields': ('active', 'title', 'parent', 'menu', 'slug') + Audit2Admin.fieldsets
-        }),
-    )
-
-    suit_form_tabs = (
-        ('page', PAGE),
-        ('language', c.LANGUAGE_TAB),
-        ('section', SECTION_PLURAL),
-    )
-
-    def _show_sections(self, obj):
-        sections = obj.sections.all()
-        html = ""
-        if sections.exists():
-            for section in sections:
-                html += f'<li><a href="/admin/contents/section/{section.id}/">{section}</a></li>'
-            return mark_safe(f'<ul>{html}</ul>')
-        return NO_SECTIONS
-
-    _show_sections.short_description = SECTION_PLURAL
-
-    def get_queryset(self, request):
-        queryset = super(__class__, self).get_queryset(request)
-        return queryset.annotate(section_count=Count('sections'))
 
 
 class CountImages(admin.SimpleListFilter):
