7c33573250e5b38595865d3a1a2230379ff06a0a
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-05-01 21:26:33 -0500

    Update settings module

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     environment = os.environ.get('ENVIRONMENT', 'development')
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectCato.settings.{}'.format(environment))
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cms.settings.{}'.format(environment))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
