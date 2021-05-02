f962f3fae7dcc32aab003f5435f15c24ccbdc7f3
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-05-01 22:27:22 -0500

    Fix S3 project root reference

--- cms/settings/production.py
+++ cms/settings/production.py
@@ -29,7 +29,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'NAME': os.path.join(CMS_DIR, 'db.sqlite3'),
         }
     }
 
